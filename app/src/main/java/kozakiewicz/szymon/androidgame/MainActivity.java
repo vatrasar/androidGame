ce12bc4ffffa70aa1a85870f2f157764dd35a121
Szymno Kozakiewicz <vatrasar@gmail.com>
2020-07-06 17:35:07 +0200

    Initial commit

---
+++ app/src/main/java/kozakiewicz/szymon/androidgame/MainActivity.java
@@ -0,0 +1,15 @@
+package kozakiewicz.szymon.androidgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int rok=3;
+    }
+}
